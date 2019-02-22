19d4eb6b2f5ad6c7a5ba120f797b1b0e4575fc1a
Pablo Brudnick <pbrudnick@gmail.com>
2019-02-22 11:35:00 -0300

    adding accounts permissions endpoints

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule BtrzExApiClient.MixProject do
   use Mix.Project
 
   @github_url "https://github.com/Betterez/btrz-ex-api-client"
-  @version "0.1.2"
+  @version "0.1.3"
 
   def project do
     [
